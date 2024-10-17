36cebd53d3903187408d1c04dc51f8cc2d1306d5
Mo0nX8 <anakinka2323@gmail.com>
2024-10-17 21:11:58 +0200

    Created 2 list, one for rooms and one for bookings is RoomController, Filled up the Rooms, each one available

--- VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/RoomController.cs
+++ VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/RoomController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VBJ._13B.Group1.WebApp.Models;
 
 namespace VBJ._13B.Group1.WebApp.Controllers
 {
@@ -7,6 +8,23 @@ namespace VBJ._13B.Group1.WebApp.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        private static List<Room> Rooms = new List<Room>();
+        private static List<Booking> Bookings = new List<Booking>();
+
+        public RoomController()
+        {
+            if (!Rooms.Any())
+            {
+                Rooms = new List<Room>
+                {
+                    new Room() { ID = 1, IsAvailable = true },
+                    new Room() { ID = 2, IsAvailable = true },
+                    new Room() { ID = 3, IsAvailable = true },
+                    new Room() { ID = 4, IsAvailable = true },
+                    new Room() { ID = 5, IsAvailable = true }
+                };
+            }
         }
     }
 }

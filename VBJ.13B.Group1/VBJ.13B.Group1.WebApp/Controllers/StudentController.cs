eb56fc06e45770bfa64c8f98994e0c26021b3316
Mo0nX8 <anakinka2323@gmail.com>
2024-10-17 20:52:23 +0200

    Created AddStudent, This gives ID and Date manually to the Student, then append it to a list and redirect to index

--- VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/StudentController.cs
+++ VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/StudentController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VBJ._13B.Group1.WebApp.Models;
 
 namespace VBJ._13B.Group1.WebApp.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly static List<Student> studentsList = new List<Student>();
         public IActionResult Index()
+        {
+            return View(studentsList);
+        }
+        public IActionResult Register()
         {
             return View();
         }
+        public IActionResult AddStudent(Student item)
+        {
+            item.ID = studentsList.Any() ? studentsList.Last().ID : 1;
+            item.date = DateTime.Now.Date;
+            studentsList.Add(item);
+            return RedirectToAction("Index");
+        }
+
     }
 }

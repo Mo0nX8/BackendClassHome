de9cb721f210deb430f74fd5a34d83cc5b61395f
Mo0nX8 <anakinka2323@gmail.com>
2024-10-17 21:02:37 +0200

    Created a list for the movies, created the Create and Edit in the MovieController

--- VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/MovieController.cs
+++ VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/MovieController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VBJ._13B.Group1.WebApp.Models;
 
 namespace VBJ._13B.Group1.WebApp.Controllers
 {
     public class MovieController : Controller
     {
+        private readonly static List<Movie> Movies = new List<Movie>();
         public IActionResult Index()
         {
+            return View(Movies);
+        }
+        public IActionResult Create()
+        {
             return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            var editedItems = Movies.FirstOrDefault(x => x.ID == id);
+            return View(editedItems);
         }
     }
 }

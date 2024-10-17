25b59add570ab938256b6a6ec82f53d7fc490653
Mo0nX8 <anakinka2323@gmail.com>
2024-10-17 21:08:49 +0200

    Created views, models and controller for Room task

---
+++ VBJ.13B.Group1/VBJ.13B.Group1.WebApp/Controllers/RoomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VBJ._13B.Group1.WebApp.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
